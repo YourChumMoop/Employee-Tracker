aefb2c8c9771227c046c900d0697ce36a19a3fff
Matt Oberdalhoff <reliable.vagabond@gmail.com>
2024-11-11 23:25:42 -0600

    added functionality to sqlQueries.ts and Cli.ts

--- src/cli/Cli.ts
+++ src/cli/Cli.ts
@@ -1,5 +1,8 @@
 import inquirer from "inquirer";
-
+import { QueryResult } from 'pg';
+import { pool, connectToDb } from '../connection.js';
+import { buildViewQ, newEmpQ } from './sqlQueries.js';
+await connectToDb();
 
 //TODO create a mainMenu() where the program asks it's first question and goes back to
     //it will have the options for VIEW ALL departments, VIEW ALL roles, VIEW ALL employees, 
@@ -27,7 +30,7 @@ class Cli {
             // Switch case statement for selecting from main menu
             switch (res.action) {
                 case 'View All Employees':
-                    //TODO add viewEmployees()
+                    this.viewEmployees();
                     break;
                 case 'Add Employee':
                     //TODO add newEmployee()
@@ -50,6 +53,69 @@ class Cli {
                 default:
                     process.exit(0);
             }
+        })
+    };
+    //
+    viewEmployees(): void {
+        //make a postgres query to select the employee table, then view it 
+        pool.quary(buildViewQ('employee') , (err: Error, result: QueryResult) => {
+            if (err) {
+                console.log(err);
+            } else if (result) {
+                console.table(result.rows);
+            }
+            this.mainMenu();
+        })
+    };
+
+    // Inquire a series of questions to build and add a new employee to the database
+    newEmployee(): void {
+        const currentRoles = pool.quary('SELECT title FROM role', (err:Error, result: QueryResult) => {
+            if (err) { 
+                console.log(err);
+            } else if (result) {
+                return result;
+            }
+        });
+        const currentEmployees = pool.quary(`SELECT CONCAT (first_name,' ',last_name) AS fullname FROM employee`, (err:Error, result: QueryResult) => {
+            if (err) { 
+                console.log(err);
+            } else if (result) {
+                return result;
+            }
+        });
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'Employee First Name: '
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'Employee Last Name: '
+            },
+            {
+                type: 'list',
+                name: 'roleID',
+                message: 'Employee Role: ',
+                choices: [currentRoles] //check SQL for role table select title 
+            },
+            {
+                type: 'list',
+                name: 'managerID',
+                message: 'Manager: ',
+                choices: [currentEmployees,'None'] //check SQL quary for all employee names
+            }
+        ]).then((res) => {
+            pool.quary(newEmpQ(res.firstName,res.lastName,res.roleID,res.managerID), (err:Error, result:QueryResult) => {
+                if (err) {
+                    console.log(err);
+                } else if (result) {
+                    console.log(`Employee ${res.firstName} ${res.lastName} Added!`);
+                }
+            })
+            
         })
     };
 };
--- src/cli/sqlQueries.ts
+++ src/cli/sqlQueries.ts
@@ -1,8 +1,15 @@
 
 //TODO functions for building quieries for SQL database
 
+export const buildViewQ = (table:string) => {
+    return `SELECT * FROM ${table}`;
+} 
+
+
 //VIEW ALL function that takes table as a param
-
 //ADD function
-
+export const newEmpQ = (first_name:string,last_name:string,role_id:string,manager_id:string) => {
+    return `INSERT INTO employee (first_name,last_name,role_id,manager_id) ` + 
+                `VALUES (${first_name},${last_name},${role_id},${manager_id};)`; 
+};
 //UPDATE function
